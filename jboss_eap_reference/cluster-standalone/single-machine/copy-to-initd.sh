ce89d32bf5d069b1ac904f4620f3219eabebff5b
Stuart Winchester <stuart.winchester@gmail.com>
2016-01-18 18:07:53 +0000

    tidy + name changes

--- jboss_eap_reference/cluster-standalone/single-machine/copy-to-initd.sh
+++ jboss_eap_reference/cluster-standalone/single-machine/copy-to-initd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo mv /vagrant/node1/jboss-as-standalone-node1.sh /etc/init.d/jboss-as-standalone-node1
-sudo mv /vagrant/node2/jboss-as-standalone-node2.sh /etc/init.d/jboss-as-standalone-node2
+sudo cp /vagrant/node1/jboss-as-standalone-node1.sh /etc/init.d/jboss-as-standalone-node1
+sudo cp /vagrant/node2/jboss-as-standalone-node2.sh /etc/init.d/jboss-as-standalone-node2
 sudo chmod +x /etc/init.d/jboss-as-standalone-node1
 sudo chmod +x /etc/init.d/jboss-as-standalone-node2
-sudo chkconfig --add jbosseap6-node1
-sudo chkconfig --add jbosseap6-node2
-sudo service jbosseap6-node1 start
-sudo service jbosseap6-node2 start
+sudo chkconfig --add jboss-as-standalone-node1
+sudo chkconfig --add jboss-as-standalone-node2
+sudo service jboss-as-standalone-node1 start
+sudo service jboss-as-standalone-node2 start
